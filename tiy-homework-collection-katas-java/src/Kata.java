93776665379cba6f21e7d39affd3f1fa788d4d35
andrewdmo <andrewdmo@gmail.com>
2016-09-05 14:42:05 -0400

    finished

--- tiy-homework-collection-katas-java/src/Kata.java
+++ tiy-homework-collection-katas-java/src/Kata.java
@@ -1,46 +1,129 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by andrewdmo on 8/17/16.
  */
 public class Kata {
 
+    static private int val;
+
     //firstLastSix
     public static boolean firstLastSix(ArrayList<Integer> al) {
-        if (al.size()<1) return false;
+        if (al.size() < 1) return false;
         int firstnum = al.get(0);
-        int lastnum = al.get(al.size()-1);
+        int lastnum = al.get(al.size() - 1);
         return ((firstnum == 6) || (lastnum == 6));
     }
+
     //sameFirstLast
     //looks like Clojure but it works
     public static boolean sameFirstLast(ArrayList<Integer> al) {
-        if ((al.size()>=1) && (al.get(0))==(al.get(al.size()-1))) {
-            return true; } {
+        if ((al.size() >= 1) && (al.get(0)) == (al.get(al.size() - 1))) {
+            return true;
+        }
+        {
             return false;
-        }}
+        }
+    }
+
     //rotateLeft3
-    //almost got working with more than 3 #s, not elegant but works
-    public ArrayList<Integer> rotateLeft3(ArrayList<Integer> al) {
-        int length = al.size();
-        int idxpos = 0;
-        for (int val = (al.get(idxpos)); idxpos<length; idxpos++, ) {
-            this.val = val;
+    //almost got working with more than 3 #s
+    public static ArrayList<Integer> rotateLeft3(ArrayList<Integer> al) {
+        al.add(al.remove(0));
+        return al;
+    }
 
-
-
+    //reverse3
+    public static ArrayList<Integer> reverse3(ArrayList<Integer> al) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        for (int i = al.size() - 1; i < al.size() && i >= 0; i--) {
+            temp.add(al.get(i));
         }
-//          int idxpos = 0;
-//            int val0 = al.get(idxpos);
-//            int val1 = al.get(idxpos+1);
-//            int val2 = al.get(idxpos+2);
-//            al.set(idxpos+2, val0);
-//            al.set(idxpos, val1);
-//            al.set(idxpos+1, val2);
-//            return al;
+        return temp;
+    }
+
+    //sumFirstPenultimate
+    public static int sumFirstPenultimate(ArrayList<Integer> al) {
+        if (al.size() >= 2) {
+            return al.get(0) + al.get(al.size() - 2);
+        } else if (al.size() == 1) {
+            return al.get(0);
+        } else {
+            return 0;
         }
+    }
+    //mapBully
+    public static HashMap<String, String> mapBully(HashMap<String, String> bl) {
+        if (bl.get("a")!=null) {
+            bl.put("b", bl.get("a"));
+            bl.put("a", "");
+        }
+        return bl;
+    }
+
+    //mapShare
+    public static HashMap<String, String> mapShare(HashMap<String, String> al) {
+        if (al.get("c")!=null) {
+            al.remove("c");
+        if (al.get("a")!=null){
+            al.put("b", al.get("a"));
+            }}
+    return al;
+    }
+
+    //mapAB
+    public static HashMap<String, String> mapAB(HashMap<String, String> al) {
+        if (al.get("a")!=null && al.get("b")!=null) {
+            al.put("ab", al.get("a")+al.get("b"));}
+            return al;
+        }
+
+    //wordLength
+    public static HashMap<String, Integer> wordLength(ArrayList<String> al) {
+        HashMap<String, Integer> wL = new HashMap<>();
+
+        for (String element : al) {
+            wL.put(element, element.length());
+        }
+
+        return wL;
+    }
+
+    public static HashMap<String, ArrayList<String>> indexWords(ArrayList<String> al) {
+        HashMap<String, ArrayList<String>> thisHash = new HashMap<>();
+
+        if((al.size() > 0) && al.get(0)!="")
+        {
+            for (int i = 0; i < al.size(); i++) {
+
+                ArrayList<String> iW = new ArrayList<>();
+
+                char key = al.get(i).charAt(0);
+
+                for (String value : al)
+                {
+                    if (value.length() <= 0) {
+                    } else if (value.charAt(0) == key) {
+                        iW.add(value);
+                    }
+                }
+                thisHash.put("" + key, iW);
+            }
+        }
+
+        return thisHash;
+    }
+
+
+
+
+
 
 
 
 }
+
+
+
+
--- tiy-homework-collection-katas-java/src/Main.java
+++ tiy-homework-collection-katas-java/src/Main.java
@@ -4,6 +4,7 @@
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -37,40 +38,234 @@ public class Main {
         al.clear();
         System.out.println("\n\nsameFirstLast:\n");
         al.clear();
-        al.addAll(Arrays.asList(1,2,3));
+        al.addAll(Arrays.asList(1, 2, 3));
         System.out.println("Kata.sameFirstLast([1,2,3]) => \"" + sfl.sameFirstLast(al) + "\"");
         al.clear();
-        al.addAll(Arrays.asList(1,2,3,1));
+        al.addAll(Arrays.asList(1, 2, 3, 1));
         System.out.println("Kata.sameFirstLast([1,2,3,1]) => \"" + sfl.sameFirstLast(al) + "\"");
         al.clear();
-        al.addAll(Arrays.asList(1,2,1));
+        al.addAll(Arrays.asList(1, 2, 1));
         System.out.println("Kata.sameFirstLast([1,2,1]) => \"" + sfl.sameFirstLast(al) + "\"");
         al.clear();
-        al.addAll(Arrays.asList(2,1,3));
+        al.addAll(Arrays.asList(2, 1, 3));
         System.out.println("Kata.sameFirstLast([2,1,3]) => \"" + sfl.sameFirstLast(al) + "\"");
         al.clear();
-        al.addAll(Arrays.asList(8,2,8));
+        al.addAll(Arrays.asList(8, 2, 8));
         System.out.println("Kata.sameFirstLast([8,2,8]) => \"" + sfl.sameFirstLast(al) + "\"");
         al.clear();
-        al.addAll(Arrays.asList(5,1,7,1,5));
+        al.addAll(Arrays.asList(5, 1, 7, 1, 5));
         System.out.println("Kata.sameFirstLast([5,1,7,1,5]) => \"" + sfl.sameFirstLast(al) + "\"");
 
         //rotateLeft3
         Kata rl3 = new Kata();
-        System.out.println("\n\nsameFirstLast:\n");
+        System.out.println("\n\nrotateLeft3:\n");
         al.clear();
-        al.addAll(Arrays.asList(1,2,3));
-        System.out.println("Kata.sameFirstLast([1,2,3]) => \"" + rl3.rotateLeft3(al) + "\"");
+        al.addAll(Arrays.asList(1, 2, 3));
+        System.out.println("Kata.rotateLeft3([1,2,3]) => \"" + rl3.rotateLeft3(al) + "\"");
         al.clear();
-        al.addAll(Arrays.asList(5,11,9));
-        System.out.println("Kata.sameFirstLast([1,2,3]) => \"" + rl3.rotateLeft3(al) + "\"");
+        al.addAll(Arrays.asList(5, 11, 9));
+        System.out.println("Kata.rotateLeft3([5,11,9]) => \"" + rl3.rotateLeft3(al) + "\"");
         al.clear();
-        al.addAll(Arrays.asList(7,0,0));
-        System.out.println("Kata.sameFirstLast([1,2,3]) => \"" + rl3.rotateLeft3(al) + "\"");
+        al.addAll(Arrays.asList(7, 0, 0));
+        System.out.println("Kata.rotateLeft3([7,0,0]) => \"" + rl3.rotateLeft3(al) + "\"");
+        al.clear();
+        al.addAll(Arrays.asList(7, 4, 7));
+        System.out.println("Kata.rotateLeft3([7,4,7]) => \"" + rl3.rotateLeft3(al) + "\"");
+        al.clear();
+        al.addAll(Arrays.asList(1, 8, 7));
+        System.out.println("Kata.rotateLeft3([1,8,7]) => \"" + rl3.rotateLeft3(al) + "\"");
+        al.clear();
+        al.addAll(Arrays.asList(0, 0, 1));
+        System.out.println("Kata.rotateLeft3([0,0,1]) => \"" + rl3.rotateLeft3(al) + "\"");
 
-//        rotateLeft3([1, 2, 3]) -> [2, 3, 1]
-//        rotateLeft3([5, 11, 9]) -> [11, 9, 5]
-//        rotateLeft3([7, 0, 0]) -> [0, 0, 7]
+        //Reverse 3
+        Kata rv3 = new Kata();
+        System.out.println("\n\nReverse3:\n");
+        al.clear();
+        al.addAll(Arrays.asList(1, 2, 3));
+        System.out.println("Kata.reverse3([1,2,3]) => \"" + rv3.reverse3(al) + "\"");
+        al.clear();
+        al.addAll(Arrays.asList(5, 11, 9));
+        System.out.println("Kata.reverse3([5,11,9]) => \"" + rv3.reverse3(al) + "\"");
+        al.clear();
+        al.addAll(Arrays.asList(7, 0, 0));
+        System.out.println("Kata.reverse3([7,0,0]) => \"" + rv3.reverse3(al) + "\"");
+        al.clear();
+        al.addAll(Arrays.asList(7, 4, 7));
+        System.out.println("Kata.reverse3([7,4,7]) => \"" + rv3.reverse3(al) + "\"");
+        al.clear();
+        al.addAll(Arrays.asList(1, 8, 7));
+        System.out.println("Kata.reverse3([1,8,7]) => \"" + rv3.reverse3(al) + "\"");
+        al.clear();
+        al.addAll(Arrays.asList(0, 0, 1));
+        System.out.println("Kata.reverse3([0,0,1]) => \"" + rv3.reverse3(al) + "\"");
 
+        //sumFirstPenultimate
+        Kata sfp = new Kata();
+        System.out.println("\n\nsumFirstPenultimate:\n");
+        al.clear();
+        al.addAll(Arrays.asList(1, 2, 3));
+        System.out.println("Kata.sumFirstPenultimate([1,2,3]) => \"" + sfp.sumFirstPenultimate(al) + "\"");
+        al.clear();
+        al.addAll(Arrays.asList(1,1));
+        System.out.println("Kata.sumFirstPenultimate([5,11,9]) => \"" + sfp.sumFirstPenultimate(al) + "\"");
+        al.clear();
+        al.addAll(Arrays.asList(1,1,1,1));
+        System.out.println("Kata.sumFirstPenultimate([7,0,0]) => \"" + sfp.sumFirstPenultimate(al) + "\"");
+        al.clear();
+        al.addAll(Arrays.asList(1,2,3,4));
+        System.out.println("Kata.sumFirstPenultimate([7,4,7]) => \"" + sfp.sumFirstPenultimate(al) + "\"");
+        al.clear();
+        al.addAll(Arrays.asList(1, 8, 7));
+        System.out.println("Kata.sumFirstPenultimate([1,8,7]) => \"" + sfp.sumFirstPenultimate(al) + "\"");
+        al.clear();
+        al.addAll(Arrays.asList(0, 0, 1));
+        System.out.println("Kata.sumFirstPenultimate([0,0,1]) => \"" + sfp.sumFirstPenultimate(al) + "\"");
+
+
+        //mapBully
+        //Kata kB = new Kata();
+        System.out.println("\n\nmapBully:\n");
+        HashMap<String, String> mB = new HashMap<>();
+        //HashMap<String, String> kB = new HashMap<>();
+        mB.put("b", "dirt");
+        mB.put("a", "candy");
+        String cB = mB.toString();
+        HashMap kB = Kata.mapBully(mB);
+        System.out.printf("Kata.mapBully(%s) => %s", cB, kB);
+
+        mB.clear();
+        kB.clear();
+        mB.put("b", "carrot");
+        mB.put("c", "meh");
+        mB.put("a", "candy");
+        cB = mB.toString();
+        kB = Kata.mapBully(mB);
+        System.out.printf("\nKata.mapBully(%s) => %s\n", cB, kB);
+
+        //mapShare
+        System.out.println("\n\nmapShare:\n");
+        HashMap<String, String> mS = new HashMap<>();
+        mS.put("b", "bbb");
+        mS.put("c", "ccc");
+        mS.put("a", "aaa");
+        String cS = mS.toString();
+        HashMap kS = Kata.mapShare(mS);
+        System.out.printf("Kata.mapShare(%s) => %s\n", cS, kS);
+
+        mS.clear();
+        kS.clear();
+        mS.put("b", "xyz");
+        mS.put("c", "ccc");
+        cS = mS.toString();
+        kS = Kata.mapShare(mS);
+        System.out.printf("Kata.mapShare(%s) => %s\n", cS, kS);
+
+        mS.clear();
+        kS.clear();
+        mS.put("d", "hi");
+        mS.put("c", "meh");
+        mS.put("a", "aaa");
+        cS = mS.toString();
+        kS = Kata.mapShare(mS);
+        System.out.printf("Kata.mapShare(%s) => %s\n", cS, kS);
+
+        //mapAB
+        System.out.println("\n\nmapAB:\n");
+        HashMap<String, String> mA = new HashMap<>();
+        mA.put("b", "There");
+        mA.put("a", "Hi");
+        String cA = mA.toString();
+        HashMap kA = Kata.mapAB(mA);
+        System.out.printf("Kata.mapAB(%s) => %s\n", cA, kA);
+
+        mA.clear();
+        kA.clear();
+        mA.put("a", "Hi");
+        cA = mA.toString();
+        kA = Kata.mapAB(mA);
+        System.out.printf("Kata.mapAB(%s) => %s\n", cA, kA);
+
+        mA.clear();
+        kA.clear();
+        mA.put("b", "There");
+        cA = mA.toString();
+        kA = Kata.mapAB(mA);
+        System.out.printf("Kata.mapAB(%s) => %s\n", cA, kA);
+
+
+        //wordLength
+        System.out.println("\n\nwordLength:\n");
+        ArrayList<String> wL = new ArrayList();
+        wL.add("a");
+        wL.add("bb");
+        wL.add("a");
+        wL.add("bb");
+        String cW = wL.toString();
+        HashMap kL = Kata.wordLength(wL);
+        System.out.printf("Kata.wordLength(%s) => %s\n", cW, kL);
+
+        wL.clear();
+        wL.add("this");
+        wL.add("and");
+        wL.add("that");
+        wL.add("and");
+        cW = wL.toString();
+        kL = Kata.wordLength(wL);
+        System.out.printf("Kata.wordLength(%s) => %s\n", cW, kL);
+
+        wL.clear();
+        wL.add("code");
+        wL.add("code");
+        wL.add("code");
+        wL.add("bug");
+        cW = wL.toString();
+        kL = Kata.wordLength(wL);
+        System.out.printf("Kata.wordLength(%s) => %s\n", cW, kL);
+
+
+        //indexWords
+        System.out.println("\n\nindexWords:\n");
+        ArrayList<String> iW = new ArrayList();
+        iW.add("aardvark");
+        iW.add("apple");
+        iW.add("zamboni");
+        iW.add("phone");
+        cW = iW.toString();
+        HashMap<String, ArrayList<String>> iL = Kata.indexWords(iW);
+        System.out.printf("Kata.wordLength(%s) => %s\n", cW, iL);
+
+        iL.clear();
+        iW.clear();
+        iW.add("boo");
+        iW.add("ya");
+        iW.add("yo");
+        iW.add("B");
+        cW = iW.toString();
+        iL = Kata.indexWords(iW);
+        System.out.printf("Kata.wordLength(%s) => %s\n", cW, iL);
+
+        iL.clear();
+        iW.clear();
+        iW.add("sunny");
+        iW.add("day");
+        iW.add("still");
+        iW.add("so");
+        iW.add("dreary");
+        cW = iW.toString();
+        iL = Kata.indexWords(iW);
+        System.out.printf("Kata.wordLength(%s) => %s\n", cW, iL);
+
+        iL.clear();
+        iW.clear();
+        iW.add("voluminous ");
+        iW.add("victory");
+        iW.add("very");
+        iW.add("vague");
+        cW = iW.toString();
+        iL = Kata.indexWords(iW);
+        System.out.printf("Kata.wordLength(%s) => %s\n", cW, iL);
+
+System.out.println("\nLa Fin!");
     }
 }
